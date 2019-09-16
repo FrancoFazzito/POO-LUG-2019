ba8091ed4d300e5c1ba6691e622a5c548c19e9e6
FrancoFazzito <48734991+FrancoFazzito@users.noreply.github.com>
2019-09-15 23:35:24 -0300

    Add comparison

---
+++ 65B COMPARISON/ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparison<int> DescComparacion = Comparacion;
+            int ValorComparacion = DescComparacion(5, 7);
+            Console.WriteLine(ValorComparacion);
+
+            List<int> numeros = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            numeros.Sort(DescComparacion);
+
+            Console.WriteLine("lista ordenada");
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+        }
+
+        static int Comparacion(int numero1, int numero2)
+        {
+            if (numero1 < numero2)
+            {
+                return 1;
+            }
+            else if(numero1 > numero2)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
