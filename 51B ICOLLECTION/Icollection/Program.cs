66430c888aa6bcedc8c4944f66b346fc9b0898dc
i7 5820K <francofazzito7@gmail.com>
2019-09-28 01:32:01 -0300

    add icollection

---
+++ 51B ICOLLECTION/Icollection/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Icollection
+{
+    class Program
+    {
+        private static int cantidad = 15;
+
+        static void Main(string[] args)
+        {
+            List<int> listaInt = new List<int>();
+            for (int i = 0; i < cantidad; i++)
+            {
+                listaInt.Add(i);
+            }
+
+            int[] arrayInt = new int[10];
+            for (int i = 0; i < arrayInt.Count(); i++)
+            {
+                arrayInt[i] = ++cantidad;
+            }
+
+            Coleccion<int> coleccion = new Coleccion<int>(listaInt);
+            coleccion.AddRange(arrayInt);
+            
+            foreach (var item in listaInt)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(coleccion[10]);
+            Console.Read();
+        }
+    }
+
+    class Coleccion<T>
+    {
+        private ICollection<T> coleccion;
+
+        public Coleccion(ICollection<T> coleccion)
+        {
+            this.coleccion = coleccion;
+        }
+
+        public void Add(T elemento)
+        {
+            this.coleccion.Add(elemento);
+        }
+        public void AddRange(ICollection elementos)
+        {
+            foreach (var elemento in elementos)
+            {
+                this.coleccion.Add((T)elemento);
+            }
+        }
+        public void Remover(T elemento)
+        {
+            this.coleccion.Remove(elemento);
+        }
+
+        public ICollection<T> GetElementos()
+        {
+            return this.coleccion;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.coleccion.ElementAt(index);
+            }
+        }
+    }
+}
---
+++ 65C PREDICATE Y FUNC/interfaces de estructuras de datos/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces_de_estructuras_de_datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            predicate();
+            funcUnParametro();
+            funcDosParametros();
+            Console.Read();
+        }
+
+        public static void predicate()
+        {
+            List<int> numeros = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Predicate<int> primeroMayorACinco = EsMayorCinco;
+            Predicate<int> primeroMayorACincoLambda = numero => numero > 5;
+            int Buscado = numeros.Find(primeroMayorACinco);
+            int BuscadoLambda = numeros.Find(primeroMayorACincoLambda);
+            Console.WriteLine(Buscado);
+            Console.WriteLine(BuscadoLambda);
+        }
+
+        public static void funcUnParametro()
+        {
+            Func<int, string> convertToString = ConvertString;
+            Func<int, string> convertToStringLambda = numero => numero.ToString();
+            string stringConvertido = convertToString(10);
+            string stringConvertidoLambda = convertToStringLambda(10);
+            Console.WriteLine(stringConvertido);
+            Console.WriteLine(stringConvertidoLambda);
+        }
+
+        public static void funcDosParametros()
+        {
+            Func<int, int, bool> sumaMayorADiezLambda = (num1, num2) => num1 + num2 > 10;
+            Func<int, int, bool> sumaMayorADiez = EsMayorDiez;
+            bool resultado = sumaMayorADiez(5, 3);
+            Console.WriteLine(resultado);
+        }
+
+        public static bool EsMayorCinco(int numero)
+        {
+            return numero > 5;
+        }
+
+        public static string ConvertString(int numero)
+        {
+            return numero.ToString();
+        }
+
+        public static bool EsMayorDiez(int num1,int num2)
+        {
+            return num1 + num2 > 10;
+        }
+    }
+
+
+}
