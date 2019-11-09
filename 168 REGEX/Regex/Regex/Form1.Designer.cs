c78346d0121bc0d0b7cc9b6f077f89649d7e897d
i7 5820K <francofazzito7@gmail.com>
2019-11-09 01:15:39 -0300

    add regex

---
+++ 168 REGEX/Regex/Regex/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace RegexValidacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.countTxt = new System.Windows.Forms.TextBox();
+            this.LenghtTxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.valueTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.countTxtNoVocal = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtCountVeces = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 105);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(238, 108);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "matches Vocal numero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "palabra";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "count";
+            // 
+            // countTxt
+            // 
+            this.countTxt.Location = new System.Drawing.Point(302, 105);
+            this.countTxt.Name = "countTxt";
+            this.countTxt.Size = new System.Drawing.Size(207, 20);
+            this.countTxt.TabIndex = 8;
+            // 
+            // LenghtTxt
+            // 
+            this.LenghtTxt.Location = new System.Drawing.Point(302, 154);
+            this.LenghtTxt.Name = "LenghtTxt";
+            this.LenghtTxt.Size = new System.Drawing.Size(207, 20);
+            this.LenghtTxt.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(262, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "lenght";
+            // 
+            // valueTxt
+            // 
+            this.valueTxt.Location = new System.Drawing.Point(302, 131);
+            this.valueTxt.Name = "valueTxt";
+            this.valueTxt.Size = new System.Drawing.Size(207, 20);
+            this.valueTxt.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(262, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "value";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 231);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "matches no Vocal";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(15, 247);
+            this.listBox2.MultiColumn = true;
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(238, 121);
+            this.listBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(302, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(207, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(262, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "index";
+            // 
+            // countTxtNoVocal
+            // 
+            this.countTxtNoVocal.Location = new System.Drawing.Point(46, 374);
+            this.countTxtNoVocal.Name = "countTxtNoVocal";
+            this.countTxtNoVocal.Size = new System.Drawing.Size(207, 20);
+            this.countTxtNoVocal.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 377);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "count";
+            // 
+            // txtCountVeces
+            // 
+            this.txtCountVeces.Location = new System.Drawing.Point(317, 374);
+            this.txtCountVeces.Name = "txtCountVeces";
+            this.txtCountVeces.Size = new System.Drawing.Size(207, 20);
+            this.txtCountVeces.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(277, 377);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "count";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(283, 227);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(126, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "matches numero 3 veces";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(286, 247);
+            this.listBox3.MultiColumn = true;
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(238, 121);
+            this.listBox3.TabIndex = 19;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 82);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 23;
+            this.checkBox1.Text = "match";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(302, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(79, 17);
+            this.checkBox2.TabIndex = 24;
+            this.checkBox2.Text = "1-0 VOCAL";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(302, 35);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(105, 17);
+            this.checkBox3.TabIndex = 25;
+            this.checkBox3.Text = "1 o MAS VOCAL";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(302, 58);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(105, 17);
+            this.checkBox4.TabIndex = 26;
+            this.checkBox4.Text = "0 o MAS VOCAL";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(430, 55);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(118, 17);
+            this.checkBox5.TabIndex = 29;
+            this.checkBox5.Text = "0 o MAS NUMERO";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(430, 32);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(118, 17);
+            this.checkBox6.TabIndex = 28;
+            this.checkBox6.Text = "1 o MAS NUMERO";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(430, 9);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(92, 17);
+            this.checkBox7.TabIndex = 27;
+            this.checkBox7.Text = "1-0 NUMERO";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 409);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtCountVeces);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.countTxtNoVocal);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.valueTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.LenghtTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.countTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox countTxt;
+        private System.Windows.Forms.TextBox LenghtTxt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox valueTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox countTxtNoVocal;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtCountVeces;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+    }
+}
+
---
+++ 168 REGEX/Regex/Regex/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace RegexValidacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ValidacionVocalNumero()
+        {
+            Regex regexVocalNumero = new Regex(@"[aeiou]\d" /*decimal*/);
+            
+            //match
+            Match resultMatch = regexVocalNumero.Match(textBox1.Text);
+            checkBox1.Checked = resultMatch.Success;
+            
+            //matches
+            listBox1.Items.Clear();
+            MatchCollection resultadosMatches = regexVocalNumero.Matches(textBox1.Text);
+            foreach (Match match in resultadosMatches)
+            {
+                listBox1.Items.Add(match.ToString());
+            }
+            
+            //textboxs de props
+            valueTxt.Text = "valor match: " + resultMatch.Value;
+            LenghtTxt.Text = "largo match: " + resultMatch.Length.ToString();
+            textBox3.Text = "match en letra numero: " + resultMatch.Index.ToString();
+            //count Matches
+            countTxt.Text = "cantidad de matches: " + resultadosMatches.Count.ToString();
+        }
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ValidacionVocalNumero();
+            ValidacionNoVocal();
+            validacionCantidadNumeros();
+            ValidacionVocales();
+            ValidacionNumero();
+            // [asd] ? 0-1 solo 1 de a-s-d
+            // [asd] + 1 o mas \d+ 
+            // [asd] * 0 o mas
+
+        }
+
+        private void ValidacionNumero()
+        {
+            Regex ceroUno = new Regex(@"\d?");
+            Regex unoMuchos = new Regex(@"\d+");
+            Regex ceroMuchos = new Regex(@"\d*");
+
+            checkBox5.Checked = ceroUno.Match(textBox1.Text).Success;
+            checkBox6.Checked = unoMuchos.Match(textBox1.Text).Success;
+            checkBox7.Checked = ceroMuchos.Match(textBox1.Text).Success;
+        }
+
+        private void ValidacionVocales()
+        {
+            Regex ceroUno = new Regex("[aeiou]?");
+            Regex unoMuchos = new Regex("[aeiou]+");
+            Regex ceroMuchos = new Regex("[aeiou]*");
+
+            checkBox2.Checked = ceroUno.Match(textBox1.Text).Success;
+            checkBox3.Checked = unoMuchos.Match(textBox1.Text).Success;
+            checkBox4.Checked = ceroMuchos.Match(textBox1.Text).Success;
+        }
+
+        private void validacionCantidadNumeros()
+        {
+            Regex regexNoVocal = new Regex(@"\d{3}");
+            listBox3.Items.Clear();
+            MatchCollection resultadosMatches = regexNoVocal.Matches(textBox1.Text);
+            foreach (Match match in resultadosMatches)
+            {
+                listBox3.Items.Add(match.ToString());
+            }
+            txtCountVeces.Text = "cantidad de matches: " + resultadosMatches.Count.ToString();
+        }
+
+        private void ValidacionNoVocal()
+        {
+            Regex regexNoVocal = new Regex("[^aeiou]");
+            listBox2.Items.Clear();
+            MatchCollection resultadosMatches = regexNoVocal.Matches(textBox1.Text);
+            foreach (Match match in resultadosMatches)
+            {
+                listBox2.Items.Add(match.ToString());
+            }
+            countTxtNoVocal.Text = "cantidad de matches: " + resultadosMatches.Count.ToString();
+        }
+    }
+}
