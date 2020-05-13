e615ad50aecd18f4d1da60bb48bab87bf941c9c7
FrancoFazzito <48734991+FrancoFazzito@users.noreply.github.com>
2020-05-13 16:46:07 -0300

    Add files via upload

---
+++ 172 Dictionary functional/dictFunc/dictFunc/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, Func<string>> diccionario = new Dictionary<string, Func<string>>();
+
+            diccionario.Add("jefe", holaJefe);
+            diccionario.Add("cliente", holaCliente);
+            diccionario.Add("empleado", holaEmpleado);
+
+            string hola = diccionario["jefe"].Invoke();
+
+            Console.WriteLine(hola);
+
+            Console.Read();
+        }
+
+        public static string holaCliente()
+        {
+            return "hola cliente";
+        }
+
+        public static string holaJefe()
+        {
+            return "hola jefe";
+        }
+
+        public static string holaEmpleado()
+        {
+            return "hola empleado";
+        }
+    }
+}
